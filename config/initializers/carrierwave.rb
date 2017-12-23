e210f49c6741e448e6cab253c620b8bb78798ebe
Garrett Forsyth <gtforsyth91@gmail.com>
2017-12-23 13:50:01 -0500

    carrierwave config change

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,8 @@
+require 'carrierwave/storage/fog'
+
 if Rails.env.production?
+  config.storage = :fog
+  config.fog_provider= 'fog/aws'
   CarrierWave.configure do |config|
     config.fog_directory     =  ENV['S3_BUCKET']
     config.fog_credentials = {
@@ -7,7 +11,5 @@ if Rails.env.production?
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.storage = :fog
-    config.fog_provider= 'fog/aws'
   end
 end
